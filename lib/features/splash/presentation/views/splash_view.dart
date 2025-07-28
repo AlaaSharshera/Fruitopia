6b1e302522e812bf78cfdabbf3a646f1c2928329
alaa <alaasharshera123@gmail.com>
2025-07-29 01:40:53 +0300

    Add localization support to SplashView and update app name display

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fruitopia/generated/l10n.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SplashView extends StatelessWidget {
@@ -9,6 +10,10 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
     body: Stack(children: [
+      Positioned(
+        top: 50,
+        right: 20,
+        child: TextButton(onPressed: (){}, child: Text(S.of(context).appLanguage,style: GoogleFonts.cairoPlay(fontSize: 18,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),)))),
       Positioned(top: 0,
       left: 0,
         child: SvgPicture.asset("assets/images/splash_plant.svg")),
@@ -16,7 +21,7 @@ class SplashView extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Image.asset('assets/images/splash.jpg',width: MediaQuery.of(context).size.width*0.8,),
-          Text("Fruitopia",style: GoogleFonts.ptSerifCaption(fontSize: 26,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),))
+          Text(S.of(context).appName,style: GoogleFonts.cairoPlay(fontSize: 26,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),))
         
         ],
       )),
