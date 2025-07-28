5971b4317dbe45d3024762f2a1901437e4895558
alaa <alaasharshera123@gmail.com>
2025-07-28 04:36:00 +0300

    Add localization support for Arabic and English
    - Generated localization files for Arabic (messages_ar.dart) and English (messages_en.dart).
    - Created messages_all.dart to manage message lookups for different locales.
    - Implemented localization delegate (AppLocalizationDelegate) for handling locale changes.
    - Added empty ARB files for Arabic and English localization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fruitopia/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
 
 void main() {
   runApp(const Fruitopia ());
@@ -9,8 +12,15 @@ class Fruitopia  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-debugShowCheckedModeBanner: false,
-      home: const Scaffold(),
+        localizationsDelegates: [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+          debugShowCheckedModeBanner: false,
+      home: SplashView(),
     );
   }
 }
