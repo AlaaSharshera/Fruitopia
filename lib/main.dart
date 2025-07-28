0a66ddf068c01caf7561d7e5c4c9459db0e26ebd
alaa <alaasharshera123@gmail.com>
2025-07-29 01:39:18 +0300

    Add device preview support and set default locale to English

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fruitopia/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'generated/l10n.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const Fruitopia ());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => Fruitopia(), 
+  ),);
 }
 
 class Fruitopia  extends StatelessWidget {
@@ -12,6 +17,10 @@ class Fruitopia  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       useInheritedMediaQuery: true,
+      //locale: DevicePreview.locale(context),
+         locale: Locale('en'),
+      builder: DevicePreview.appBuilder,
         localizationsDelegates: [
                 S.delegate,
                 GlobalMaterialLocalizations.delegate,
