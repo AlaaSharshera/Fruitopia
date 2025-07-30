637a10ed92dfef2613b15feb411df087c439004f
alaa <alaasharshera123@gmail.com>
2025-07-30 05:01:49 +0300

    Add routeName constant to SplashView

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -8,6 +8,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
+  static const String routeName = '/splash';
 
   @override
   Widget build(BuildContext context) {
