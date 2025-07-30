afd76634a3af323be870cdedec82d26ca3283497
alaa <alaasharshera123@gmail.com>
2025-07-30 05:58:54 +0300

    Refactor SplashView to StatefulWidget and implement navigation to OnboardingView

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:fruitopia/features/on_boarding/presentation/views/onboarding_view.dart';
 import 'package:fruitopia/features/splash/presentation/widgets/splash_view_body.dart';
 
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
   static const String routeName = '/splash';
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+   implementNavigation();
+    super.initState();
+  }
+  @override
+ 
   Widget build(BuildContext context) {
     return Scaffold(
     body: SplashViewBody()
     );
   }
+  
+ void implementNavigation() {
+  Future.delayed(Duration(seconds: 3), () {
+    if (!mounted) return; 
+    Navigator.pushReplacementNamed(context, OnboardingView.routeName);
+  });
+}
+
 }
