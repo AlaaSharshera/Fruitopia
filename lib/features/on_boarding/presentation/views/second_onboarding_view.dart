06d4644afb98c47fc8434ec4c2c862d7a6ee035b
alaa <alaasharshera123@gmail.com>
2025-07-31 01:08:52 +0300

    Add SecondOnboardingView widget for onboarding flow

---
+++ lib/features/on_boarding/presentation/views/second_onboarding_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:fruitopia/core/utils/app_images.dart';
+import 'package:fruitopia/features/on_boarding/presentation/widgets/custom_onboarding_body.dart';
+import 'package:fruitopia/generated/l10n.dart';
+
+class SecondOnboardingView extends StatelessWidget {
+  const SecondOnboardingView({super.key});
+  static const String routeName = '/Secondonboarding';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomOnboardingBody(
+        isOnboarding1: false,
+        title: S.of(context).onboardingTitle2,
+        subtitle: S.of(context).onboardingSubtitle2,
+        backgroundColor: Color(0xffB0E8C7),
+        image: Assets.imagesPineappleOnboarding,
+      ),
+    );
+  }
+}
