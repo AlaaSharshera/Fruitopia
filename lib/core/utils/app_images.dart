7e330635f44fa2d21feeda1ffed5df8e18400e43
alaa <alaasharshera123@gmail.com>
2025-07-30 05:24:39 +0300

    Add app_images.dart for asset management and update pubspec.yaml for asset configuration

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String iconsLogo = "assets/icons/logo.png";
+
+  static const String imagesOnboardingFruitBasket = "assets/images/onboarding-fruit-basket.svg";
+
+  static const String imagesSplash = "assets/images/splash.avif";
+
+  static const String imagesSplashBottom = "assets/images/splash-bottom.svg";
+
+  static const String imagesSplashPlant = "assets/images/splash_plant.svg";
+}
+
