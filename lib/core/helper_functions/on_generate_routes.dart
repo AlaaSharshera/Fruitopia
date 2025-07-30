5cde27349f35c74499f5d26bdf00028f53ad09db
alaa <alaasharshera123@gmail.com>
2025-07-30 05:38:16 +0300

    Add route for OnboardingView in onGenerateRoutes

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fruitopia/features/on_boarding/presentation/views/onboarding_view.dart';
 import 'package:fruitopia/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoutes(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnboardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnboardingView());
     
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
