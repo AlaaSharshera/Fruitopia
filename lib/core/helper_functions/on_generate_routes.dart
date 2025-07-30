29f086af248d91c0d81dfdaab8bda134d7e30d55
alaa <alaasharshera123@gmail.com>
2025-07-31 01:09:43 +0300

    Refactor route generation to include FirstOnboardingView and SecondOnboardingView, removing obsolete OnboardingView

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:fruitopia/features/on_boarding/presentation/views/onboarding_view.dart';
+import 'package:fruitopia/features/on_boarding/presentation/views/first_onboarding_view.dart';
+import 'package:fruitopia/features/on_boarding/presentation/views/onboarding_pageview.dart';
+import 'package:fruitopia/features/on_boarding/presentation/views/second_onboarding_view.dart';
 import 'package:fruitopia/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoutes(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
-    case OnboardingView.routeName:
-      return MaterialPageRoute(builder: (context) => const OnboardingView());
-    
+    case FirstOnboardingView.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const FirstOnboardingView(),
+      );
+    case SecondOnboardingView.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const SecondOnboardingView(),
+      );
+case OnboardingPageview.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const OnboardingPageview(),
+      );
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
 }
-
-
