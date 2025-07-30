b68b948c069728f0dea8d541b128d031221d880b
alaa <alaasharshera123@gmail.com>
2025-07-30 05:31:20 +0300

    Refactor SplashViewBody to use asset constants for image paths

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fruitopia/core/cubits/app_language_cubit/app_language_cubit.dart';
+import 'package:fruitopia/core/utils/app_images.dart';
 import 'package:fruitopia/generated/l10n.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -20,17 +21,17 @@ class SplashViewBody extends StatelessWidget {
         }, child: Text(S.of(context).appLanguage,style: GoogleFonts.cairoPlay(fontSize: 20.sp,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),)))),
       Positioned(top: 0,
       left: 0,
-        child: SvgPicture.asset("assets/images/splash_plant.svg",width: MediaQuery.of(context).size.width*0.5,height: MediaQuery.of(context).size.height*0.2,)),
+        child: SvgPicture.asset(Assets.imagesSplashPlant,width: MediaQuery.of(context).size.width*0.5,height: MediaQuery.of(context).size.height*0.2,)),
       Center(child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/splash.jpg',width: MediaQuery.of(context).size.width*0.8,),
+          Image.asset(Assets.imagesSplash,width: MediaQuery.of(context).size.width*0.8),
           Text(S.of(context).appName,style: GoogleFonts.cairoPlay(fontSize: 26.sp,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),))
         
         ],
       )),
       Positioned(bottom: 0,
-        child: SvgPicture.asset('assets/images/splash-bottom.svg',width: MediaQuery.of(context).size.width)),
+        child: SvgPicture.asset(Assets.imagesSplashBottom,width: MediaQuery.of(context).size.width)),
       
     ],);
   }
