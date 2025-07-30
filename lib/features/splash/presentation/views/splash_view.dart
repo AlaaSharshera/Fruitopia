56afcba85479ec9cc2c3afe2e0bd452024b74c14
alaa <alaasharshera123@gmail.com>
2025-07-30 03:25:53 +0300

    Refactor SplashView to use ScreenUtil for responsive design and update text styles

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fruitopia/generated/l10n.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,22 +12,22 @@ class SplashView extends StatelessWidget {
     return Scaffold(
     body: Stack(children: [
       Positioned(
-        top: 50,
-        right: 20,
-        child: TextButton(onPressed: (){}, child: Text(S.of(context).appLanguage,style: GoogleFonts.cairoPlay(fontSize: 18,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),)))),
+        top: 50.h,
+        right: 20.w ,
+        child: TextButton(onPressed: (){}, child: Text(S.of(context).appLanguage,style: GoogleFonts.cairoPlay(fontSize: 20.sp,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),)))),
       Positioned(top: 0,
       left: 0,
-        child: SvgPicture.asset("assets/images/splash_plant.svg")),
+        child: SvgPicture.asset("assets/images/splash_plant.svg",width: MediaQuery.of(context).size.width*0.5,height: MediaQuery.of(context).size.height*0.2,)),
       Center(child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Image.asset('assets/images/splash.jpg',width: MediaQuery.of(context).size.width*0.8,),
-          Text(S.of(context).appName,style: GoogleFonts.cairoPlay(fontSize: 26,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),))
+          Text(S.of(context).appName,style: GoogleFonts.cairoPlay(fontSize: 26.sp,fontWeight: FontWeight.bold,color: Color(0xff1B5E37),))
         
         ],
       )),
-      Positioned(bottom: 50,
-        child: SvgPicture.asset('assets/images/splash-bottom.svg'))
+      Positioned(bottom: 50.h,
+        child: SvgPicture.asset('assets/images/splash-bottom.svg',width: MediaQuery.of(context).size.width)),
       
     ],),
     );
