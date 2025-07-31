452d456bd4c1f21d9d5d56f99bca096ac42a6a78
alaa <alaasharshera123@gmail.com>
2025-07-31 21:01:17 +0300

    Refactor OnboardingPageview to use StatefulWidget and implement page navigation with CustomPageIndicator

--- lib/features/on_boarding/presentation/views/onboarding_pageview.dart
+++ lib/features/on_boarding/presentation/views/onboarding_pageview.dart
@@ -1,16 +1,71 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruitopia/features/on_boarding/presentation/views/first_onboarding_view.dart';
 import 'package:fruitopia/features/on_boarding/presentation/views/second_onboarding_view.dart';
+import 'package:fruitopia/features/on_boarding/presentation/widgets/custom_button.dart';
+import 'package:fruitopia/features/on_boarding/presentation/widgets/custom_page_indicator.dart';
+import 'package:fruitopia/generated/l10n.dart';
 
-class OnboardingPageview extends StatelessWidget {
+class OnboardingPageview extends StatefulWidget {
   const OnboardingPageview({super.key});
-static const String routeName = '/onboardingPageview';
+  static const String routeName = '/onboardingPageview';
+
+  @override
+  State<OnboardingPageview> createState() => _OnboardingPageviewState();
+}
+
+class _OnboardingPageviewState extends State<OnboardingPageview> {
+  late PageController pageController;
+  int pageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: const [
-        FirstOnboardingView(),
-        SecondOnboardingView(),
+    return Column(
+      children: [
+        Expanded(
+          child: PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              setState(() {
+                pageIndex = index;
+                log("page index $pageIndex", name: pageIndex.toString());
+              });
+            },
+            children: const [FirstOnboardingView(), SecondOnboardingView()],
+          ),
+        ),
+        SizedBox(height: 20.h),
+        CustomPageIndicator(
+          pageIndex: pageIndex,
+          pageController: pageController,
+        ),
+
+        Visibility(
+          visible: pageIndex == 1 ? true : false,
+          maintainSize: true,
+          maintainAnimation: true,
+          maintainState: true,
+          child: Padding(
+            padding: EdgeInsets.all(16.r),
+            child: CustomButton(
+              text: S.of(context).onboardingButtonText,
+              onPressed: () {},
+            ),
+          ),
+        ),
       ],
     );
   }
