daef8cc986f0216d71e83b788623fa61b61cf563
alaa <alaasharshera123@gmail.com>
2025-07-31 20:59:55 +0300

    Add CustomPageIndicator widget for enhanced onboarding experience

---
+++ lib/features/on_boarding/presentation/widgets/custom_page_indicator.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fruitopia/constants.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class CustomPageIndicator extends StatelessWidget {
+  const CustomPageIndicator({required this.pageController,required this.pageIndex,super.key});
+   final PageController pageController;
+    final int pageIndex ;
+
+  @override
+  Widget build(BuildContext context) {
+    return    SmoothPageIndicator(    
+   controller: pageController,    
+   count:  2,    
+   axisDirection: Axis.horizontal,    
+   effect:  ColorTransitionEffect(    
+      spacing:  6.w,    
+      radius:  4.r,    
+      dotWidth:  8.w,    
+      dotHeight:  8.h,    
+      paintStyle:  PaintingStyle.fill,      
+      dotColor:  pageIndex==1?kPrimaryColor:Color(0xff5DB957).withOpacity(0.5),    
+      activeDotColor:  kPrimaryColor    
+  ), 
+);
+  }
+}
