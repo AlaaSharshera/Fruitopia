37d46b1071ced3a2303b2fbc901e0779ddcc4d09
alaa <alaasharshera123@gmail.com>
2025-08-01 10:14:23 +0300

    Persists the selected app language (English or Arabic) to shared preferences.

--- lib/core/cubits/app_language_cubit/app_language_cubit.dart
+++ lib/core/cubits/app_language_cubit/app_language_cubit.dart
@@ -1,15 +1,21 @@
 import 'dart:ui';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruitopia/constants.dart';
+import 'package:fruitopia/core/services/shared_prefrences_singleton.dart';
 
 class AppLanguageCubit extends Cubit<Locale>{
   AppLanguageCubit():super(Locale('en'));
   Locale appLanguage=Locale('en');
+  
+  
   void changeLanguage(){
     if(appLanguage.languageCode=='en'){
       appLanguage=Locale('ar');
+      Prefs.setString(kAppLanguage, appLanguage.languageCode);
     }else{
       appLanguage=Locale('en');
+      Prefs.setString(kAppLanguage, appLanguage.languageCode);
     }
     emit(appLanguage);
   }
