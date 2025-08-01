a44d0c69a005209c0156fb20f887e7ef7e06e8a0
alaa <alaasharshera123@gmail.com>
2025-08-01 10:12:28 +0300

    Implements a check to determine whether the user has already seen the onboarding screen.

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fruitopia/constants.dart';
+import 'package:fruitopia/core/services/shared_prefrences_singleton.dart';
+import 'package:fruitopia/features/auth/presentation/views/login_view.dart';
 import 'package:fruitopia/features/on_boarding/presentation/views/onboarding_pageview.dart';
 import 'package:fruitopia/features/splash/presentation/widgets/splash_view_body.dart';
 
@@ -27,9 +30,10 @@ class _SplashViewState extends State<SplashView> {
   }
   
  void implementNavigation() {
+  bool isonBoardingViewSeen = Prefs.getBool(kIsOnBoardingViewSeen);
   Future.delayed(Duration(seconds: 3), () {
     if (!mounted) return; 
-    Navigator.pushReplacementNamed(context, OnboardingPageview.routeName);
+  Navigator.pushReplacementNamed(context, isonBoardingViewSeen? LoginView.routeName : OnboardingPageview.routeName);
   });
 }
 
