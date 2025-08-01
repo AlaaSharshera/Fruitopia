73ed60d855e0e9ea9e395574c816881f107a2034
alaa <alaasharshera123@gmail.com>
2025-08-01 10:14:58 +0300

    Initializes shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fruitopia/app.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:fruitopia/core/services/shared_prefrences_singleton.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Prefs.init();
   runApp(DevicePreview(
     enabled: !kReleaseMode,
     builder: (context) => Fruitopia(), 
