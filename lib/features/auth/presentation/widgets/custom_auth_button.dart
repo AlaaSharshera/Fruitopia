d72d0ba0a43ec1f4f71a3c730fb4e1d88c3364ca
alaa <alaasharshera123@gmail.com>
2025-08-03 00:23:55 +0300

    Adds a reusable custom authentication button component for authentication flows.

---
+++ lib/features/auth/presentation/widgets/custom_auth_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomAuthButton extends StatelessWidget {
+  const CustomAuthButton({super.key, required this.buttonText, required this.buttonImage});
+  final String buttonText;
+  final String buttonImage;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 45.h,
+      margin: EdgeInsets.symmetric(horizontal: 16.w ,vertical: 6.h),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.r),
+        border: Border.all(color: const Color(0xFFE6E9E9), width: 1.w),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        SizedBox(width: 20.w),
+        Image.asset(buttonImage),
+        Spacer(),
+        Text(buttonText,style:GoogleFonts.cairo(
+          color: const Color(0xFF0C0D0D) ,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w600,
+        )),
+       SizedBox(width: MediaQuery.of(context).size.width * 0.25),
+      ],)
+    );
+  }
+}
