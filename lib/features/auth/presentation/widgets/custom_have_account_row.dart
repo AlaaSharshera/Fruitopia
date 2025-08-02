6b876c4c9286a6f20b0375686327607dec042bd5
alaa <alaasharshera123@gmail.com>
2025-08-03 02:59:02 +0300

    Adds a reusable widget to display a row of text indicating whether the user already has an account, along with an action text to navigate to the appropriate screen.

---
+++ lib/features/auth/presentation/widgets/custom_have_account_row.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomHaveAccountRow extends StatelessWidget {
+  const CustomHaveAccountRow({
+    required this.text,
+    required this.actionText,
+    required this.onTap,
+    super.key,
+  });
+  final String text;
+  final String actionText;
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            text,
+            style: GoogleFonts.cairo(
+              color: const Color(0xFF616A6B),
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          GestureDetector(
+            onTap: onTap,
+            child: Text(
+              actionText,
+              style: GoogleFonts.cairo(
+                color: const Color(0xFF1B5E37),
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
