d4b80f2a0479123040a8fc342e80d8d88f44376e
alaa <alaasharshera123@gmail.com>
2025-08-03 00:25:35 +0300

    Adds a custom forget password button widget.

---
+++ lib/features/auth/presentation/widgets/custom_forgetpass_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fruitopia/constants.dart';
+import 'package:fruitopia/core/services/shared_prefrences_singleton.dart';
+import 'package:fruitopia/generated/l10n.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Widget customForgetPassButton(BuildContext context, {required VoidCallback onPressed}) {
+  return  Container(
+            alignment: Prefs.getString(kAppLanguage)=='ar' ? Alignment.centerLeft : Alignment.centerRight,
+            margin: EdgeInsets.symmetric(horizontal: 16.w),
+            child: TextButton(
+              onPressed: onPressed,
+              child: Text(
+                S.of(context).forgetPassword,
+                style: GoogleFonts.cairo(
+                  color: const Color(0xFF2D9F5D),
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          );
+}
