a06f126bfaa94328ecd3b465fc3c55b74511afbc
alaa <alaasharshera123@gmail.com>
2025-08-02 21:47:44 +0300

    Improves responsiveness

--- lib/core/widgets/custom_appbar.dart
+++ lib/core/widgets/custom_appbar.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 PreferredSizeWidget customAppBar(BuildContext context, { required String  title}) {
   return AppBar(
     centerTitle: true,
     leading: Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding:  EdgeInsets.all(8.r),
       child: CircleAvatar(
         backgroundColor: const Color(0xFFF1F1F5),
-        radius: 20,
+        radius: 20.r,
         child: CircleAvatar(
           backgroundColor: Colors.white,
-          radius: 19,
+          radius: 19.r,
           child: IconButton(
-            icon: const Icon(Icons.arrow_back_ios_new, size: 18, color: Colors.black),
+            icon:  Icon(Icons.arrow_back_ios_new, size: 20.r, color: Colors.black),
             onPressed: () => Navigator.of(context).pop(),
           ),
         ),
