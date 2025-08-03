4fc2c66187cf5205502378f7b374bd673b3eb830
alaa <alaasharshera123@gmail.com>
2025-08-04 01:16:47 +0300

    Moves the terms and conditions checkbox and text into a dedicated widget. This improves code organization and reusability.

--- lib/features/auth/presentation/widgets/signup_body.dart
+++ lib/features/auth/presentation/widgets/signup_body.dart
@@ -3,8 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruitopia/core/widgets/custom_button.dart';
 import 'package:fruitopia/core/widgets/custom_textformfield.dart';
 import 'package:fruitopia/features/auth/presentation/widgets/custom_have_account_row.dart';
+import 'package:fruitopia/features/auth/presentation/widgets/terms_conditions_widget.dart';
 import 'package:fruitopia/generated/l10n.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 class SignUpBody extends StatelessWidget {
   const SignUpBody({super.key});
@@ -27,49 +28,7 @@ class SignUpBody extends StatelessWidget {
             obscureText: true,
             suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
           ),
-          Padding(
-            padding:  EdgeInsets.all(8.h),
-            child: Row(
-              children: [
-                Checkbox(
-                  value: false,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4.r),
-                  ),
-                  side: BorderSide(
-                    color: const Color(0xFFDCDEDE),
-                    width: 1.w,
-                  ),
-                  onChanged: (value) {},
-                  activeColor: const Color(0xFF2D9F5D),
-                ),
-                Expanded(
-                  child: Text.rich(
-                    TextSpan(
-                      children: [
-                        TextSpan(
-                          text: S.of(context).termsOfService,
-                        style: GoogleFonts.cairo(
-                          color: const Color(0xFF949D9E),
-                          fontSize: 13,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      TextSpan(
-                        text: S.of(context).acceptTerms,
-                        style: GoogleFonts.cairo(
-                          color: const Color(0xFF2D9F5D),
-                          fontSize: 13,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.left,
-                              ),
-                ),]
-            ),
-          ),
+          TermsAndConditionsWidget(),
           Padding(
             padding: EdgeInsets.all(8.r),
             child: CustomButton(text: S.of(context).registerButtonText,onPressed:() {
@@ -88,3 +47,4 @@ class SignUpBody extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/features/auth/presentation/widgets/terms_conditions_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:fruitopia/features/auth/presentation/widgets/custom_checked_box.dart';
+import 'package:fruitopia/generated/l10n.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TermsAndConditionsWidget extends StatefulWidget {
+  const TermsAndConditionsWidget({super.key});
+
+  @override
+  State<TermsAndConditionsWidget> createState() => _TermsAndConditionsWidgetState();
+}
+
+class _TermsAndConditionsWidgetState extends State<TermsAndConditionsWidget> {
+  bool isTermsAccepted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          CustomCheckedBox(isChecked: isTermsAccepted,onChanged: (value) {
+            setState(() {
+              isTermsAccepted = value;
+            });
+          },),
+          Expanded(
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: S.of(context).termsOfService,
+                    style: GoogleFonts.cairo(
+                      color: const Color(0xFF949D9E),
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text: S.of(context).acceptTerms,
+                    style: GoogleFonts.cairo(
+                      color: const Color(0xFF2D9F5D),
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
