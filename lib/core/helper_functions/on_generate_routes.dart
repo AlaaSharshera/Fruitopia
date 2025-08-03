774561cf33074a9154eb5ddf12ba2b4f092654dc
alaa <alaasharshera123@gmail.com>
2025-08-03 05:06:26 +0300

    Adds navigation routes for the ForgetPassView, NewPassView, and OtpView within the authentication flow.

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fruitopia/features/auth/presentation/views/forget_pass_view.dart';
 import 'package:fruitopia/features/auth/presentation/views/login_view.dart';
+import 'package:fruitopia/features/auth/presentation/views/new_pass_view.dart';
+import 'package:fruitopia/features/auth/presentation/views/otp_view.dart';
 import 'package:fruitopia/features/auth/presentation/views/sign_up_view.dart';
 import 'package:fruitopia/features/on_boarding/presentation/views/first_onboarding_view.dart';
 import 'package:fruitopia/features/on_boarding/presentation/views/onboarding_pageview.dart';
@@ -26,7 +29,13 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const LoginView());
     case SignUpView.routeName:
       return MaterialPageRoute(builder: (context) => const SignUpView());
-    default:
+      case ForgetPassView.routeName:
+      return MaterialPageRoute(builder: (context) => const ForgetPassView());
+      case NewPassView.routeName:
+      return MaterialPageRoute(builder: (context) => const NewPassView());
+    case OtpView.routeName:
+      return MaterialPageRoute(builder: (context) => const OtpView());
+     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
 }
