f48f237738842abbf5ff49587919d4777aa7e067
alaa <alaasharshera123@gmail.com>
2025-08-03 05:12:15 +0300

    Adds new password view

---
+++ lib/features/auth/presentation/views/new_pass_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fruitopia/constants.dart';
+import 'package:fruitopia/core/services/shared_prefrences_singleton.dart';
+import 'package:fruitopia/core/widgets/custom_appbar.dart';
+import 'package:fruitopia/core/widgets/custom_button.dart';
+import 'package:fruitopia/core/widgets/custom_textformfield.dart';
+import 'package:fruitopia/generated/l10n.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class NewPassView extends StatelessWidget {
+  const NewPassView({super.key});
+  static const routeName = '/new-pass-view';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar(context, title: S.of(context).newPasswordTitle),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16.r),
+              child: Text(
+                S.of(context).newPasswordSubtitle,
+                textAlign: Prefs.getString(kAppLanguage) == 'ar'
+                    ? TextAlign.right
+                    : TextAlign.left,
+                style: GoogleFonts.cairo(
+                  color: const Color(0xFF0C0D0D),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 16.h),
+              child: CustomTextFormField(
+                hintText: "",
+                obscureText: true,
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffC9CECF),
+                ),
+              ),
+            ),
+            CustomTextFormField(
+              hintText: "",
+              obscureText: true,
+              suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
+            ),
+            Padding(
+              padding:  EdgeInsets.all(16.r),
+              child: CustomButton(text: S.of(context).createNewPassword),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
