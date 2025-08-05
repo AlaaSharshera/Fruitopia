b84d59a479b30ea2751aad78acc7beeaaf087cf6
alaa <alaasharshera123@gmail.com>
2025-08-05 22:20:34 +0300

    Provides the `SignupCubit` to the `SignUpView`.

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruitopia/core/services/get_it_service.dart';
 import 'package:fruitopia/core/widgets/custom_appbar.dart';
+import 'package:fruitopia/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruitopia/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
 import 'package:fruitopia/features/auth/presentation/widgets/signup_body.dart';
 import 'package:fruitopia/generated/l10n.dart';
 
@@ -9,9 +13,11 @@ class SignUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return BlocProvider(create: (context)=>SignupCubit(
+      getIt<AuthRepo>(),
+    ),child: Scaffold(
       appBar: customAppBar(context, title: S.of(context).registerTitle),
       body: SignUpBody(),
-    );
+    ),);
   }
 }
