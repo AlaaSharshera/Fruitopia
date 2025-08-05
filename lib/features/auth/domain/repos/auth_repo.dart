5686048f69fe0e849e8f2f01b6dfaecb5f6c3e35
alaa <alaasharshera123@gmail.com>
2025-08-05 20:48:04 +0300

    Updates createUser to accept name

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -3,7 +3,7 @@ import 'package:fruitopia/core/errors/failures.dart';
 import 'package:fruitopia/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<Either<Failures, UserEntity>> createUserWithEmailAndPassword(String email, String password)  {
+  Future<Either<Failures, UserEntity>> createUserWithEmailAndPassword(String email, String password,String name)  {
     throw UnimplementedError();
   }
 
