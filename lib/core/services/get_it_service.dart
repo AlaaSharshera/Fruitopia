c48aeb30be4945b721bf044e9ad0cf44e3998be6
alaa <alaasharshera123@gmail.com>
2025-08-05 23:24:45 +0300

    edit the name of the setup function to setupgetIt

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -5,7 +5,7 @@ import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
-void setup() {
+void setupgetIt() {
   getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
   getIt.registerSingleton<AuthRepo>(
     AuthRepoImplementation(firebaseAuthService: getIt<FirebaseAuthService>()));
