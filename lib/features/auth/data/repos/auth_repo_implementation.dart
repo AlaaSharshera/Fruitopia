40760a25b47353d306c821de7c83a7e7126af738
alaa <alaasharshera123@gmail.com>
2025-08-06 05:09:20 +0300

    Adds the implementation for user sign-in functionality using email and password.

--- lib/features/auth/data/repos/auth_repo_implementation.dart
+++ lib/features/auth/data/repos/auth_repo_implementation.dart
@@ -8,20 +8,50 @@ import 'package:fruitopia/features/auth/data/models/user_model.dart';
 import 'package:fruitopia/features/auth/domain/entities/user_entity.dart';
 import 'package:fruitopia/features/auth/domain/repos/auth_repo.dart';
 
-class AuthRepoImplementation implements AuthRepo{
+class AuthRepoImplementation implements AuthRepo {
   final FirebaseAuthService firebaseAuthService;
   AuthRepoImplementation({required this.firebaseAuthService});
   @override
-  Future<Either<Failures, UserEntity>> createUserWithEmailAndPassword(String email, String password,String name) async {
+  Future<Either<Failures, UserEntity>> createUserWithEmailAndPassword(
+    String email,
+    String password,
+    String name,
+  ) async {
     try {
-      final user = await firebaseAuthService.createUserWithEmailAndPassword(email: email, password: password);
+      final user = await firebaseAuthService.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
       return Right(UserModel.fromFirebaseUser(user));
     } on CustomException catch (e) {
       return Left(ServerFailure(e.message));
-    }catch (e) {
-      log("Exception in AuthRepoImplementation.createUserWithEmailAndPassword: ${e.toString()}");
+    } catch (e) {
+      log(
+        "Exception in AuthRepoImplementation.createUserWithEmailAndPassword: ${e.toString()}",
+      );
       return Left(ServerFailure(e.toString()));
     }
   }
 
+  @override
+  Future<Either<Failures, UserEntity>> signInWithEmailAndPassword(
+    String email,
+    String password,
+    String name,
+  ) async {
+    try {
+      final user = await firebaseAuthService.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return Right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      return Left(ServerFailure(e.message));
+    } catch (e) {
+      log(
+        "Exception in AuthRepoImplementation.signInWithEmailAndPassword in catch block: ${e.toString()}",
+      );
+      return Left(ServerFailure(e.toString()));
+    }
+  }
 }
