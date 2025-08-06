51b27dcfd4ab7a71dd04dd8309096c0517d13b1b
alaa <alaasharshera123@gmail.com>
2025-08-06 03:03:21 +0300

    Improves app debugging and initialization
    Configures the app for better debugging by introducing a custom bloc observer that logs bloc state changes. It also initializes getIt service in main.dart.

--- lib/core/services/custom_bloc_observer.dart
+++ lib/core/services/custom_bloc_observer.dart
@@ -1,15 +1,17 @@
+import 'dart:developer';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomBlocObserver extends BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    print('${bloc.runtimeType} $change');
+    log('${bloc.runtimeType} $change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print('${bloc.runtimeType} ${transition.currentState} => ${transition.nextState}');
+    log('${bloc.runtimeType} ${transition.currentState} => ${transition.nextState}');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruitopia/app.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:fruitopia/core/services/custom_bloc_observer.dart';
+import 'package:fruitopia/core/services/get_it_service.dart';
 import 'package:fruitopia/core/services/shared_prefrences_singleton.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  Bloc.observer = CustomBlocObserver();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  setupgetIt();
   await Prefs.init();
-  runApp(DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => Fruitopia(), 
-  ),);
+  runApp(
+    DevicePreview(enabled: !kReleaseMode, builder: (context) => Fruitopia()),
+  );
 }
-
