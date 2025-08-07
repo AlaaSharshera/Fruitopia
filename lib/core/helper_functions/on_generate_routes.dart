01773c9c2fd480455ab57b4af790e01f73fce44c
alaa <alaasharshera123@gmail.com>
2025-08-07 06:50:51 +0300

    provide SigninCubit

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruitopia/core/services/get_it_service.dart';
+import 'package:fruitopia/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruitopia/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
 import 'package:fruitopia/features/auth/presentation/views/forget_pass_view.dart';
 import 'package:fruitopia/features/auth/presentation/views/signin_view.dart';
 import 'package:fruitopia/features/auth/presentation/views/new_pass_view.dart';
 import 'package:fruitopia/features/auth/presentation/views/otp_view.dart';
 import 'package:fruitopia/features/auth/presentation/views/sign_up_view.dart';
-import 'package:fruitopia/features/auth/presentation/widgets/signin_view_body.dart';
 import 'package:fruitopia/features/bottom_nav_bar/presentation/widgets/bottom_nav_bar.dart' ;
 import 'package:fruitopia/features/on_boarding/presentation/views/first_onboarding_view.dart';
 import 'package:fruitopia/features/on_boarding/presentation/views/onboarding_pageview.dart';
@@ -28,7 +31,8 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
         builder: (context) => const OnboardingPageview(),
       );
     case SigninView.routeName:
-      return MaterialPageRoute(builder: (context) => const SigninViewBody());
+      return MaterialPageRoute(builder: (context) => 
+      BlocProvider<SigninCubit>(create: (context) => SigninCubit(getIt<AuthRepo>()), child: const SigninView()));
     case SignUpView.routeName:
       return MaterialPageRoute(builder: (context) => const SignUpView());
       case ForgetPassView.routeName:
